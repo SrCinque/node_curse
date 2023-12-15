bd5db590831eac625082ff171691ae41fc6f8fef
Filipe Amaro <97483586+SrCinque@users.noreply.github.com>
2023-12-15 17:20:17 +0000

    ajustes de TASKS

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
+const tasks = require('./routes/tasks')
 
 
 app.get('/hello', (req, res) => {
     res.send('foi');
 })
 
+
+app.use('/api/v1/tasks', tasks)
+
 const port = 3000
 
-app.listen(port, console.log('server is listering on port ...'))
+app.listen(port, console.log(`server is listering on ${port} ...`))
