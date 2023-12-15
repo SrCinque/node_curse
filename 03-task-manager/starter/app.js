ac26303e108379e87894a03cd5f254d5eda6b304
Filipe Amaro <97483586+SrCinque@users.noreply.github.com>
2023-12-15 16:27:52 +0000

    ajuste

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,11 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+
+
+app.get('/hello', (req, res) => {
+    res.send('foi');
+})
+
+const port = 3000
+
+app.listen(port, console.log('server is listering on port ...'))
